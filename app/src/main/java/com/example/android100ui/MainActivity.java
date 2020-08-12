4e3c3f3cf6bc8aac5f1901f4ace70384d991cf12
Guan Wang <judew@fb.com>
2020-08-12 11:17:31 -0400

    init

---
+++ app/src/main/java/com/example/android100ui/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.android100ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    EditText textbox;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.submit_button);
+        textbox = findViewById(R.id.textbox);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handleClick();
+            }
+        });
+    }
+
+    private void handleClick() {
+        String text = textbox.getText().toString();
+        Toast toast = Toast.makeText(this, "Hello " + text, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
