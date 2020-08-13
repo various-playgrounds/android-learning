cc76fe5f0eca3eb0588ad129e586c07d6a9ef803
Guan Wang <judew@fb.com>
2020-08-13 10:16:07 -0400

    integrate first lesson with second one

--- app/src/main/java/com/example/android100ui/PageFragment.java
+++ app/src/main/java/com/example/android100ui/PageFragment.java
@@ -4,7 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
@@ -12,6 +15,7 @@ public class PageFragment extends Fragment {
     public static final String ARG_PAGE = "ARG_PAGE";
 
     private int mPage;
+    EditText textbox;
 
     public static PageFragment newInstance(int page) {
         Bundle args = new Bundle();
@@ -30,9 +34,28 @@ public class PageFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_page, container, false);
-        TextView textView = (TextView) view;
-        textView.setText("Fragment #" + mPage);
+
+        View view;
+        if (mPage == 1) {
+            view = inflater.inflate(R.layout.fragment_page_new, container, false);
+
+            Button button = view.findViewById(R.id.submit_button);
+            textbox = view.findViewById(R.id.textbox);
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String text = textbox.getText().toString();
+                    Toast toast = Toast.makeText(PageFragment.this.getContext(), "Hello " + text, Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+            });
+        } else {
+            view = inflater.inflate(R.layout.fragment_page, container, false);
+            TextView textView = (TextView) view;
+            textView.setText("Fragment #" + mPage);
+        }
+
+
         return view;
     }
 }
